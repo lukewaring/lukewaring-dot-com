8ee6820dec80ad1d7ad11cbb20e8a65641cdf392
lukewaring <lukewaring@gmail.com>
2020-04-07 14:09:36 -0400

    Add Gatsby styled-components plugin; revise copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default () => (
     <Wrapper>
       <Header>Luke Waring</Header>
       <br></br>
-      <Paragraph>Software developer based in Carroll Gardens, Brooklyn</Paragraph>
+      <Paragraph>Software developer based in Carroll Gardens, Brooklyn. I work primarily with React and related technologies, but I'm always learning and seek to use the right tool for the job at hand.</Paragraph>
       <br></br>
       <Paragraph><a href="https://github.com/lukewaring" target="_blank" rel="noopener noreferrer">GitHub</a></Paragraph>
       <Paragraph><a href="https://www.linkedin.com/in/lukewaring" target="_blank" rel="noopener noreferrer">LinkedIn</a></Paragraph>
@@ -33,6 +33,5 @@ export default () => (
       <Paragraph><a href="https://twitter.com/luke_waring" target="_blank" rel="noopener noreferrer">Twitter</a></Paragraph>
       <br></br>
       <Paragraph>Built with <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a> and deployed with <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Netlify</a></Paragraph>
-      <Paragraph>Design based on the 2019 <a href="https://2019.stateofjs.com" target="_blank" rel="noopener noreferrer">State of JavaScript</a> survey</Paragraph>
     </Wrapper>
 )
