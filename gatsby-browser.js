611693841f3e93bf7d3da720df7795472e2e85f0
lukewaring <lukewaring@gmail.com>
2020-04-07 17:25:41 -0400

    Add Google fonts in CSS and gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,3 @@
 import "./src/styles/global.css"
+import "typeface-titillium-web"
+import "typeface-crete-round"
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,14 @@ export default () => (
       <h1>Luke Waring</h1>
       <br></br>
       <p>Software developer based in Carroll Gardens, Brooklyn</p>
-      <p>I work primarily with React and related technologies, but I'm always learning and seek to use the right tool for the job at hand</p>
+      <p>I work primarily with React and related technologies, but I'm always learning and seek to use the right tool for the job at hand.</p>
       <br></br>
-      <p><a href="https://github.com/lukewaring" target="_blank" rel="noopener noreferrer">GitHub</a></p>
-      <p><a href="https://linkedin.com/in/lukewaring" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-      <p><a href="https://medium.com/@lukewaring" target="_blank" rel="noopener noreferrer">Medium</a></p>
-      <p><a href="https://twitter.com/luke_waring" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+      <b><a href="https://github.com/lukewaring" target="_blank" rel="noopener noreferrer">GitHub</a></b>
+      <br></br>
+      <b><a href="https://linkedin.com/in/lukewaring" target="_blank" rel="noopener noreferrer">LinkedIn</a></b>
+      <br></br>
+      <b><a href="https://medium.com/@lukewaring" target="_blank" rel="noopener noreferrer">Medium</a></b>
+      <br></br>
+      <b><a href="https://twitter.com/luke_waring" target="_blank" rel="noopener noreferrer">Twitter</a></b>
     </main>
 )
