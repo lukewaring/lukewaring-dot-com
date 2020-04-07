a09398c5ca6973884289d316d54c517f342d6338
lukewaring <lukewaring@gmail.com>
2020-04-07 16:15:43 -0400

    Remove styled-components plugin from gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,5 @@
  */
 
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
-  ]
+  /* Your site config here */
 }
