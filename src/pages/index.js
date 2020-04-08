40a38d67a677af52136dfc9290a8c9190a8fabfb
lukewaring <lukewaring@gmail.com>
2020-04-07 22:42:30 -0400

    Reworked alignment and placement; revised html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
 import React from "react"
 
 export default () => (
-    <main>
-      <h1>Luke Waring</h1>
-      <br></br>
-      <p>Software developer based in Carroll Gardens, Brooklyn</p>
-      <p>I work primarily with React and related technologies, but I'm always learning and seek to use the right tool for the job at hand.</p>
-      <br></br>
-      <br></br>
-      <b><a href="https://github.com/lukewaring" target="_blank" rel="noopener noreferrer">GitHub</a></b>
-      <br></br>
-      <b><a href="https://linkedin.com/in/lukewaring" target="_blank" rel="noopener noreferrer">LinkedIn</a></b>
-      <br></br>
-      <b><a href="https://medium.com/@lukewaring" target="_blank" rel="noopener noreferrer">Medium</a></b>
-      <br></br>
-      <b><a href="https://twitter.com/luke_waring" target="_blank" rel="noopener noreferrer">Twitter</a></b>
-    </main>
+    <div>
+        <h1>Luke Waring</h1>
+        <p>Software developer based in Carroll Gardens, Brooklyn</p>
+        <p>I work primarily with React and related technologies, but I'm always learning and seek to use the right tool for the job at hand.</p>
+        <div><b><a href="https://github.com/lukewaring" target="_blank" rel="noopener noreferrer">GitHub</a></b></div>
+        <div><b><a href="https://linkedin.com/in/lukewaring" target="_blank" rel="noopener noreferrer">LinkedIn</a></b></div>
+        <div><b><a href="https://medium.com/@lukewaring" target="_blank" rel="noopener noreferrer">Medium</a></b></div>
+        <div><b><a href="https://twitter.com/luke_waring" target="_blank" rel="noopener noreferrer">Twitter</a></b></div>
+    </div>
 )
