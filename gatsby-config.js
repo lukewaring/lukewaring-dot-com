3c81cffd3bc0b4cc055ae7c88978bf550e3e3e64
lukewaring <lukewaring@gmail.com>
2020-05-28 12:46:23 -0400

    Add GoatCounter analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    {
+      resolve: `gatsby-plugin-react-helmet`
+    },
+    {
+      resolve: `gatsby-plugin-goatcounter`,
+      options: {
+        code: '7510'
+      }
+    }
+  ]
 }
