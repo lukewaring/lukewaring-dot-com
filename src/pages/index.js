4a5fe529f978d9a9d96a27b1f1a7c4378fcffd83
lukewaring <lukewaring@gmail.com>
2020-08-08 10:39:00 -0400

    Update links

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ export default () => (
     <p>I work primarily with JavaScript, React and Ruby on Rails, but I am always learning and seek to use the right tool for the job at hand.</p>
     <p>I love building user-friendly software that solves real business problems.</p>
     <div><b><a href='https://github.com/lukewaring' target='_blank' rel='noopener noreferrer'>GitHub</a></b></div>
-    <div><b><a href='https://linkedin.com/in/lukewaring' target='_blank' rel='noopener noreferrer'>LinkedIn</a></b></div>
     <div><b><a href='https://medium.com/@lukewaring' target='_blank' rel='noopener noreferrer'>Medium</a></b></div>
+    <div><b><a href='https://linkedin.com/in/lukewaring' target='_blank' rel='noopener noreferrer'>LinkedIn</a></b></div>
     <div><b><a href='https://twitter.com/luke_waring' target='_blank' rel='noopener noreferrer'>Twitter</a></b></div>
     <div><b><a href='https://www.notion.so/waring/49a3a012aff946bbacc0bed6d46df098?v=57524a1c9aad4b26a2381eacd6127392' target='_blank' rel='noopener noreferrer'>Reading List</a></b></div>
   </div>
