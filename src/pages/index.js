5b1b33aafaea6a811a3fa81c545fad2fbb6d7a90
lukewaring <lukewaring@gmail.com>
2020-10-27 19:14:25 -0400

    Tweak bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,16 @@ export default () => (
   <div>
     <Helmet htmlAttributes={{ lang: 'en' }}>
       <title>Luke Waring</title>
-      <meta name='description' content='Personal website of Luke Waring, a software developer based in Brooklyn, NY' />
+      <meta name='description' content='Personal website of Luke Waring, a software engineer based in Brooklyn, NY' />
       <meta name='author' content='Luke Waring' />
       <meta name='title' property='og:title' content='Luke Waring' />
       <meta name='type' property='og:type' content='website' />
       <meta name='image' property='og:image' content='https://pbs.twimg.com/profile_images/1039214074838507520/Y_7zGRWT_400x400.jpg' />
       <meta name='url' property='og:url' content='https://www.lukewaring.com/' />
-      <meta property='og:description' content='Personal website of Luke Waring, a software developer based in Brooklyn, NY' />
+      <meta property='og:description' content='Personal website of Luke Waring, a software engineer based in Brooklyn, NY' />
     </Helmet>
     <h1>Luke Waring</h1>
-    <h2>Software developer based in Brooklyn, New York</h2>
+    <h2>Software engineer based in Brooklyn, New York</h2>
     <p>I work primarily with JavaScript, React and Ruby on Rails, but I am always learning and seek to use the right tool for the job at hand.</p>
     <p>I love building user-friendly software that solves real problems.</p>
     <div><b><a href='https://github.com/lukewaring' target='_blank' rel='noopener noreferrer'>GitHub</a></b></div>
