974291554e3b6b177fc317b5758c2d544d226859
Luke Waring <33970417+lukewaring@users.noreply.github.com>
2021-07-10 17:43:40 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,10 @@ export default () => (
       <meta property='og:description' content='Personal website of Luke Waring, a software engineer based in Brooklyn, NY' />
     </Helmet>
     <h1>Luke Waring</h1>
-    <h2>Software engineer based in Brooklyn, New York</h2>
-    <p>I work primarily with JavaScript, React and Ruby on Rails, but I am always learning and seek to use the right tool for the job at hand.</p>
-    <p>I love building user-friendly software that solves real problems.</p>
+    <h2>Software Engineer at Spring Health</h2>
+    <h2>Brooklyn, New York</h2>
+    <p>I work primarily with JavaScript, React, GraphQL and Ruby on Rails, but I am always learning and seek to use the right tool for the job at hand.</p>
+    <p>I love working with good people to build user-friendly software that solves real problems.</p>
     <div><b><a href='https://github.com/lukewaring' target='_blank' rel='noopener noreferrer'>GitHub</a></b></div>
     <div><b><a href='https://medium.com/@lukewaring' target='_blank' rel='noopener noreferrer'>Medium</a></b></div>
     <div><b><a href='https://linkedin.com/in/lukewaring' target='_blank' rel='noopener noreferrer'>LinkedIn</a></b></div>
